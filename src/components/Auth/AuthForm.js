6054883810d10c69633ae5e6c26cea07e13eb235
KRSNaveen <krsnaveen876@gmail.com>
2023-12-26 23:31:39 +0530

    loading

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,51 @@
+import { useState, useRef } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLog, setLogin] = useState(true);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+    setLogin(true);
+  };
+
+  const clickHandler = () =>{
+    setLogin(false);
+    setIsLogin(false);
+  }
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form >
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+          />
+        </div>
+        <div className={classes.actions}>
+         {/* {isLogin && <button onClick={clickHandler}>Login</button>}  */}
+         { isLog ?  <button onClick={clickHandler}>Sign Up</button>:  "Sending request......."}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
